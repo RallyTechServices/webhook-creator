c70886829ca15d12160ce30483259ac6b7560d31
John Martin <curmudgeon+git@rallydev.com>
2017-10-27 15:51:37 -0700

    start over

--- test/fast/fast-templater.js
+++
@@ -1,8 +0,0 @@
-var template = './test/fast/templates/custom.tmpl';
-
-exports.process = function(grunt, task, context) {
-	
-    var source = grunt.file.read(template);
-    return grunt.util._.template(source, context);
-
-};
